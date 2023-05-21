4b60be544fad9f7f54ac720352b4b06cb634b97d
Luis Alberto Patiño Scozzese <luisalbertoscozzese@gmail.com>
2023-05-21 10:35:40 -0400

    Add files via upload

---
+++ ClientSignalR/ClientSignalR/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System.Xml;
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("https://localhost:7035/MainHub")
+    .Build();
+
+//connection.On<string>("ClientMessage", message =>
+//{
+//    Console.WriteLine(message);
+//});
+
+await connection.StartAsync();
+
+while (true)
+{
+    var message = Console.ReadLine();
+    await connection.InvokeAsync("AddToCompanyGroup","1");
+}
